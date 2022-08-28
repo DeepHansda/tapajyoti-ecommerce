9c5d5660b181456631d391dcb41fadacae6cf13d
Deep Hansda <deephansda921@gmail.com>
2022-08-28 10:25:19 +0530

    changes in navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,12 @@ import Navbar from './Components/Navbar/Navbar';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
+
 function App() {
+
   const [openBar,setOpenBar] = useState(false)
+
+  
   return (
     <div className="App">
       <Navbar openBar={openBar} setOpenBar={setOpenBar}/>
